e78d46865f4f9a9a5839255ebde2207d51ec5e61
Sushant <sushatsinghchauhan08@gmail.com>
2024-03-12 17:05:35 +0530

    Made code more modular

--- src/main/java/com/sushant/cartservice/services/FakeStoreCartServices.java
+++ src/main/java/com/sushant/cartservice/services/FakeStoreCartServices.java
@@ -18,12 +18,7 @@ public class FakeStoreCartServices implements CartService {
     @Override
     public Cart getSingleCart(Long id) {
         FakeStoreCart fakeStoreCart = restTemplate.getForObject("https://fakestoreapi.com/carts/"+id, FakeStoreCart.class);
-        Cart cart = new Cart();
-        cart.setId(fakeStoreCart.getId());
-        cart.setUserId(fakeStoreCart.getUserId());
-        cart.setDate(fakeStoreCart.getDate());
-        cart.setProducts(fakeStoreCart.getProducts());
-        return cart;
+        return copy(fakeStoreCart);
     }
 
     @Override
@@ -31,12 +26,7 @@ public class FakeStoreCartServices implements CartService {
         FakeStoreCart[] fakeStoreCarts = restTemplate.getForObject("https://fakestoreapi.com/carts", FakeStoreCart[].class);
         ArrayList<Cart> carts = new ArrayList<>();
         for (FakeStoreCart fakeStoreCart : fakeStoreCarts) {
-            Cart cart = new Cart();
-            cart.setId(fakeStoreCart.getId());
-            cart.setUserId(fakeStoreCart.getUserId());
-            cart.setDate(fakeStoreCart.getDate());
-            cart.setProducts(fakeStoreCart.getProducts());
-            carts.add(cart);
+            carts.add(copy(fakeStoreCart));
         }
         return carts;
     }
@@ -46,12 +36,7 @@ public class FakeStoreCartServices implements CartService {
         FakeStoreCart[] fakeStoreCarts = restTemplate.getForObject("https://fakestoreapi.com/carts?startdate="+startDate+"&enddate="+endDate, FakeStoreCart[].class);
         ArrayList<Cart> carts = new ArrayList<>();
         for (FakeStoreCart fakeStoreCart : fakeStoreCarts) {
-            Cart cart = new Cart();
-            cart.setId(fakeStoreCart.getId());
-            cart.setUserId(fakeStoreCart.getUserId());
-            cart.setDate(fakeStoreCart.getDate());
-            cart.setProducts(fakeStoreCart.getProducts());
-            carts.add(cart);
+            carts.add(copy(fakeStoreCart));
         }
         return carts;
     }
@@ -61,33 +46,15 @@ public class FakeStoreCartServices implements CartService {
         FakeStoreCart[] fakeStoreCarts = restTemplate.getForObject("https://fakestoreapi.com/carts/user/"+id, FakeStoreCart[].class);
         ArrayList<Cart> carts = new ArrayList<>();
         for (FakeStoreCart fakeStoreCart : fakeStoreCarts) {
-            Cart cart = new Cart();
-            cart.setId(fakeStoreCart.getId());
-            cart.setUserId(fakeStoreCart.getUserId());
-            cart.setDate(fakeStoreCart.getDate());
-            cart.setProducts(fakeStoreCart.getProducts());
-            carts.add(cart);
+            carts.add(copy(fakeStoreCart));
         }
         return carts;
     }
 
     @Override
     public Cart createCart(FakeStoreAddCart cart) {
-        FakeStoreAddCart fakeStoreAddCart = new FakeStoreAddCart();
-        fakeStoreAddCart.setUserId(cart.getUserId());
-        fakeStoreAddCart.setDate(cart.getDate());
-        fakeStoreAddCart.setProducts(cart.getProducts());
-        FakeStoreCart gotCart = restTemplate.postForObject("https://fakestoreapi.com/carts", fakeStoreAddCart, FakeStoreCart.class);
+        FakeStoreCart gotCart = restTemplate.postForObject("https://fakestoreapi.com/carts", cart , FakeStoreCart.class);
         return copy(gotCart);
-    }
-
-    public Cart copy(FakeStoreCart fakeStoreCart) {
-        Cart cart = new Cart();
-        cart.setId(fakeStoreCart.getId());
-        cart.setUserId(fakeStoreCart.getUserId());
-        cart.setDate(fakeStoreCart.getDate());
-        cart.setProducts(fakeStoreCart.getProducts());
-        return cart;
     }
 
     @Override
@@ -107,5 +74,13 @@ public class FakeStoreCartServices implements CartService {
         return getSingleCart(id);
     }
 
+    public Cart copy(FakeStoreCart fakeStoreCart) {
+        Cart cart = new Cart();
+        cart.setId(fakeStoreCart.getId());
+        cart.setUserId(fakeStoreCart.getUserId());
+        cart.setDate(fakeStoreCart.getDate());
+        cart.setProducts(fakeStoreCart.getProducts());
+        return cart;
+    }
 
 }
